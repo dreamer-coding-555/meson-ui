1b2ec3b9619b58ae82b5853bd2f49eec70c31215
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2023-09-02 23:45:57 -0600

    Update program.py

--- code/program.py
+++ code/program.py
@@ -8,84 +8,113 @@ import tkinter as tk
 import subprocess
 import threading
 import queue
+import os
 
 class MesonBuildGUI:
     def __init__(self, root):
         self.root = root
-        self.root.title("Meson Build GUI")
+        self.root.title("Meson-UI")
+        self.root.configure(bg="gray30")
+
+        # Get the current working directory
+        self.default_path = os.getcwd()
+
+        # Create a Canvas for the modern Meson Build-style logo
+        self.canvas = tk.Canvas(root, width=120, height=80, bg="gray30", highlightthickness=0)
+        self.canvas.grid(row=0, column=0, padx=10, pady=10, sticky="nw")
+        
+        # Draw a simplified representation of the modern Meson Build logo
+        self.canvas.create_text(60, 20, text="Meson-UI", fill="green", font=("Arial", 20, "bold"))
 
         # Project Path Entry
-        self.path_label = tk.Label(root, text="Project Path:")
-        self.path_label.pack()
-        self.path_entry = tk.Entry(root, width=40)
-        self.path_entry.pack()
+        self.path_label = tk.Label(root, text="Project Path:", bg="gray30", fg="gray80", font=("Arial", 10, "bold"))
+        self.path_label.grid(row=0, column=0, padx=5, pady=5, sticky="e")
+        self.path_entry = tk.Entry(root, width=80)
+        self.path_entry.grid(row=0, column=1, padx=5, pady=5)
+        self.path_entry.insert(0, self.default_path)  # Auto-fill with current working directory
 
-        # Buttons
-        self.setup_button = tk.Button(root, text="Setup", command=self.setup_project)
-        self.setup_button.pack()
-        self.compile_button = tk.Button(root, text="Compile", command=self.compile_project)
-        self.compile_button.pack()
-        self.test_button = tk.Button(root, text="Test", command=self.test_project)
-        self.test_button.pack()
-        self.init_button = tk.Button(root, text="Init Project", command=self.init_project)
-        self.init_button.pack()
+        # Button Frame
+        self.button_frame = tk.Frame(root, bg="gray40")
+        self.button_frame.grid(row=1, column=0, columnspan=2, padx=5, pady=5)
 
-        # Progress Bar
-        self.progress_bar = tk.Progressbar(root, orient="horizontal", length=300, mode="determinate")
-        self.progress_bar.pack()
+        # Buttons (arranged using grid)
+        button_style = {'padx': 25, 'pady': 5, 'bg': 'gray40', 'fg': 'gray80'}
+        self.setup_button = tk.Button(self.button_frame, text="Setup Project", command=self.setup_project, **button_style)
+        self.setup_button.grid(row=0, column=0)
+        
+        # Buttons for compiling, initializing, running tests, and cleaning
+        self.compile_button = tk.Button(self.button_frame, text="Compile Project", command=self.compile_project, **button_style)
+        self.compile_button.grid(row=0, column=1)
+        
+        self.init_button = tk.Button(self.button_frame, text="Init Project", command=self.initialize_project, **button_style)
+        self.init_button.grid(row=0, column=2)
+        
+        self.run_tests_button = tk.Button(self.button_frame, text="Run Tests", command=self.run_tests, **button_style)
+        self.run_tests_button.grid(row=0, column=3)
+        
+        self.clean_button = tk.Button(self.button_frame, text="Clean Project", command=self.clean_project, **button_style)
+        self.clean_button.grid(row=0, column=4)
 
         # Output Console
-        self.console = tk.Text(root, wrap="none")
-        self.console.pack()
+        self.console = tk.Text(root, wrap="none", bg="black", fg="white")
+        self.console.grid(row=2, column=0, columnspan=2, padx=5, pady=5, sticky="nsew")
+        self.console.tag_configure("stdout", foreground="green")
+        self.console.tag_configure("stderr", foreground="red")
 
         # Command Queue
         self.command_queue = queue.Queue()
 
-    def update_console(self, text):
-        self.console.insert(tk.END, text)
+
+    def update_console(self, text, tag=None):
+        self.console.insert(tk.END, text, tag)
         self.console.see(tk.END)
 
     def run_command(self, command):
         try:
             process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
             for line in process.stdout:
-                self.command_queue.put(line)
+                self.command_queue.put(("stdout", line))
             for line in process.stderr:
-                self.command_queue.put(line)
+                self.command_queue.put(("stderr", line))
             process.wait()
         except Exception as e:
-            self.command_queue.put(f"Error: {e}\n")
+            self.command_queue.put(("stderr", f"Error: {e}\n"))
 
     def execute_command(self, command):
-        self.command_queue.put(f"Running command: {command}\n")
+        self.command_queue.put(("stdout", f"Running command: {command}\n"))
         self.run_command(command)
-        self.command_queue.put("Command finished.\n")
+        self.command_queue.put(("stdout", "Command finished.\n"))
 
     def setup_project(self):
         project_path = self.path_entry.get()
-        command = f"meson setup {project_path}/build"
+        command = f"meson setup {project_path}/builddir"
         self.execute_command(command)
 
     def compile_project(self):
         project_path = self.path_entry.get()
-        command = f"ninja -C {project_path}/build"
+        command = f"ninja -C {project_path}/builddir"
         self.execute_command(command)
 
-    def test_project(self):
-        project_path = self.path_entry.get()
-        command = f"ninja -C {project_path}/build test"
-        self.execute_command(command)
-
-    def init_project(self):
+    def initialize_project(self):
         project_path = self.path_entry.get()
         command = f"meson init {project_path}"
+        self.execute_command(command)
+
+    def run_tests(self):
+        project_path = self.path_entry.get()
+        command = f"meson test -C {project_path}/builddir"
+        self.execute_command(command)
+
+    def clean_project(self):
+        project_path = self.path_entry.get()
+        command = f"ninja -C {project_path}/builddir clean"
         self.execute_command(command)
 
     def update_progress(self):
         while True:
             try:
-                line = self.command_queue.get(timeout=0.1)
-                self.update_console(line)
+                tag, line = self.command_queue.get(timeout=0.1)
+                self.update_console(line, tag)
                 self.root.update()
             except queue.Empty:
                 if not threading.active_count() > 1:
@@ -95,7 +124,6 @@ class MesonBuildGUI:
         progress_thread = threading.Thread(target=self.update_progress)
         progress_thread.daemon = True
         progress_thread.start()
-
 
 def greet():
     return "Hello, Python Developer"
